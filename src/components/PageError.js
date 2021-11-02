efe5f49a41f71efea46465f95a2a89ad42b5e45f
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2021-11-02 13:03:54 -0500

    Fixes, added firebase and automerge actions

--- src/components/PageError.js
+++ src/components/PageError.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles/PageError.css';
 
 function PageError(props) {
-  return <div className="PageError">❌{props.error.message}😱</div>;
+  return <div className="PageError"><span role="img" aria-label="x emoji">❌</span>{props.error.message}<span role="img" aria-label="screaming emoji">😱</span></div>;
 }
 
 export default PageError;
